d240b1d0eaecaba84e29f55934f773b6c1944df7
plh <phuong.le@elca.vn>
2023-04-13 09:57:05 +0700

    add script to install neovim

---
+++ install_neovim.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Check if Neovim is already installed
+if command -v nvim >/dev/null 2>&1 ; then
+    echo "Neovim is already installed."
+else
+    # Install Neovim
+    echo "Installing Neovim..."
+    sudo apt-get update
+    sudo apt-get install neovim
+fi
