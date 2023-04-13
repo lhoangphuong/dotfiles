b74083a36bb59757c66fd4569fd27d30eee2b8e7
plh <phuong.le@elca.vn>
2023-04-13 10:12:07 +0700

    update neovim install script

--- install_neovim.sh
+++ install_neovim.sh
@@ -6,6 +6,7 @@ if command -v nvim >/dev/null 2>&1 ; then
 else
     # Install Neovim
     echo "Installing Neovim..."
-    sudo apt-get update -y
-    sudo apt-get install neovim -y
+    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+    chmod +x nvim.appimage
+    sudo mv nvim.appimage /usr/local/bin/nvim
 fi
